0c680205386fd1cc6df5dad75024b30b58fe2bfb
Peter Renström <peter.renstrom@edgeware.tv>
2015-06-18 11:26:34 +0200

    Rename to mockito-edgeware and bump version to 1.0.0.

--- setup.py
+++ setup.py
@@ -6,22 +6,23 @@ extra = {}
 if sys.version_info >= (3,):
   extra['use_2to3'] = True
 
-setup(name='mockito',
-      version='0.5.1',
+setup(name='mockito-edgeware',
+      version='1.0.0',
       packages=['mockito', 'mockito_test', 'mockito_util'],
-      url='http://code.google.com/p/mockito-python',
-      download_url='http://code.google.com/p/mockito-python/downloads/list',
+      url='https://github.com/edgeware/mockito-python',
+      download_url='http://pypi.edgeware.tv/simple/mockito-edgeware',
       maintainer='Mockito Maintainers',
       maintainer_email='mockito-python@googlegroups.com',
       license='MIT',
       description='Spying framework',
       long_description='Mockito is a spying framework based on Java library with the same name.',
-      classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Topic :: Software Development :: Testing',
-                   'Programming Language :: Python :: 3'
-                  ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Testing',
+          'Programming Language :: Python :: 3'
+      ],
       test_suite = 'nose.collector',
       py_modules = ['distribute_setup'],
       setup_requires = ['nose'],
